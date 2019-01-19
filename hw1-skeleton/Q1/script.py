7e7f12b4af20772a6d395bcd2334900fccf4c232
Prerna Jain <pjain310@gatech.edu>
2019-01-19 16:20:48 -0500

    Added solution for 1.1.c as well

--- hw1-skeleton/Q1/script.py
+++ hw1-skeleton/Q1/script.py
@@ -6,6 +6,9 @@ import time
 import sys
 import collections
 
+#Start timer
+st = time.time()
+
 #Parse user input to get API
 api_key=sys.argv[1]
 
@@ -14,10 +17,18 @@ conn = http.client.HTTPSConnection("api.themoviedb.org")
 
 payload = "{}"
 
-#start from page 1
+######----------------------------------------------------------------------------------------------------------------######
+######-------------------------------------------------- PART 1.1.b --------------------------------------------------######
+######----------------------------------------------------------------------------------------------------------------######
+#Desc: retrieve 350 most popular movies from Drama genre released in 2004 or after
+
+#start from page 1, initialise counters at 0
 page_no=1
 res_no=0
 rcount=0
+
+#Initialise dict to store movie id and movie title (for part c)
+movies_dict={}
 
 #Open file to write results down
 f=open("movie_ID_name.csv","w")
@@ -38,7 +49,8 @@ while res_no<=360:
     for movie in results["results"]:
         if rcount<350:
             rcount+=1
-            print("{},{}".format(movie["id"],movie["title"]))
+            f.write("{},{}\n".format(movie["id"],movie["title"]))
+            movies_dict[movie["id"]]=movie["title"]
         else:
             break
 
@@ -47,3 +59,54 @@ while res_no<=360:
     res_no+=len(results["results"])
 
 f.close()
+
+
+######----------------------------------------------------------------------------------------------------------------######
+######-------------------------------------------------- PART 1.1.c --------------------------------------------------######
+######----------------------------------------------------------------------------------------------------------------######
+#Desc: Retrieve 5 similar movies for each of the movies in part 1.1.b
+
+#Initialise a set which stores similar movies
+s_movies_set=set()
+
+#Initialise query counter (starts at 20 since 19 queries have already been made for 1.1.b). Once 40 queries are done, sleep for 10 seconds
+q_count=20
+
+#Loop through each movie in movies_dict
+for id in movies_dict:
+
+    q_count+=1
+    conn.request("GET", "/3/movie/{}/similar?page=1&language=en-US&api_key={}".format(id,api_key), payload)
+
+    res = conn.getresponse()
+    data = res.read()
+
+    #Convert from json format to dict
+    results=json.loads(data)
+
+    #Initialise s_count to store first 5 results only
+    s_count=0
+
+    for movie in results["results"]:
+        if s_count<5:
+            s_count+=1
+            row="{},{}".format(min(movies_dict[id],movie["title"]),max(movies_dict[id],movie["title"]))
+            s_movies_set.add(row)
+        else:
+            break
+
+    #If 40 queries are done, refresh query counter and sleep for 10 secs
+    if q_count>=40:
+        q_count=1
+        time.sleep(10)
+
+#Sort and store list of movies
+s_movies_list=sorted(list(s_movies_set))
+
+#Open file for similar movies
+with open("movie_ID_sim_movie_ID.csv","w") as f:
+    for entry in s_movies_list:
+        f.write("{}\n".format(entry))
+
+#Print elapsed time
+#print("Time elapsed: {}".format(time.time()-st))
